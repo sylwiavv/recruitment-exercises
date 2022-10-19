463a361b62c08745b2101eebab050bf40f51d2e1
Sylwia Mentel <sylwiam@Sylwias-MacBook-Pro.local>
2022-10-20 01:33:11 +0200

    Add inline editing solution

--- src/js/inline_editing/index.js
+++ src/js/inline_editing/index.js
@@ -1,6 +1,8 @@
-const inlineEditingWrapper = document.querySelector('#inline_editing-wrapper');
 const artificialInput = document.querySelector('.artificial-input');
 const body = document.querySelector('.inline_editing');
+
+const currentColor = localStorage.getItem('artificialInput');
+artificialInput.innerHTML = currentColor;
 
 body.addEventListener('click', (e) => {
     artificialInput.contentEditable = 'true';
@@ -8,16 +10,21 @@ body.addEventListener('click', (e) => {
     if (!e.target.matches('.artificial-input') ) {
         artificialInput.contentEditable = 'false';
     }
-})
+});
 
 artificialInput.addEventListener('keydown', (e) => {
     if (e.keyCode === 27) {
         artificialInput.contentEditable = 'false';
     }
-})
+});
 
 artificialInput.addEventListener('input', (e) => {
     if (artificialInput.isContentEditable) {
-        const test = artificialInput.textContent;
+        const artificialInputValue = artificialInput.textContent;
+        saveContent(artificialInputValue);
     }
-})
+});
+
+const saveContent = (content) => {
+    localStorage.setItem('artificialInput', content);
+}
